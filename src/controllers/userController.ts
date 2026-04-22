1f7ad8a03c99f79004454d1010938a4024253da0
Ajuu01 <raieajuna1005@gmail.com>
2026-04-22 22:27:37 +0545

    password encryption and login feature added

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request,Response } from "express";
 import User from "../database/models/userModel"
+import bcrypt from 'bcrypt'
 
 class userController{
     static async Register(req:Request,res:Response){
@@ -15,13 +16,44 @@ class userController{
         const user=await User.create({
             username,
             email,
-            password,
+            password:bcrypt.hashSync(password,10),
         })
         res.status(200).json({
             message:"User registered successfully!!"
         })
     }
+    static async Login(req:Request,res:Response){
+        const {email,password}=req.body
+        if(!email || !password){
+            res.status(200).json({
+                message:"Please provide email and password"
+            })
+            return
+        }
 
+        const [user]=await User.findAll({
+            where:{
+                email:email
+            }
+        })
+
+        if(!user){
+            res.status(200).json({
+                message:"No user with that email found"
+            })
+        }else{
+            const isEqual=bcrypt.compareSync(password,user.password)
+            if(!isEqual){
+                res.status(400).json({
+                    message:"Incorrect Password"
+                })
+            }else{
+                res.status(200).json({
+                    message:"Logged in successfully"
+                })
+            }
+        }
+    }
 }
 
 export default userController
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import userController from "../controllers/userController";
 const userRouter:Router=express.Router()
 
 userRouter.route("/register").post(userController.Register)
+userRouter.route("/login").post(userController.Login)
 
 export default userRouter
